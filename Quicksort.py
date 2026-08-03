dc9aea48525c4bebe8568bacf7462b69155ce265
vishnusaikamalpachala <saikamalchowdary30@gmail.com>
2026-08-03 22:55:09 +0530

    Rename quicksort function to quick_sort

--- Quicksort.py
+++ Quicksort.py
@@ -1,7 +1,14 @@
-def quicksort(arr):
-    if len(arr) <= 1: return arr
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+
+    return left + middle + right
+
+arr = [10, 7, 8, 9, 1, 5]
+sorted_arr = quick_sort(arr)
+print("Sorted array:", sorted_arr)
